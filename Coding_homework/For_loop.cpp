a6da063a6af6db657e6ff12ccaa77ee199027b92
oreorider <yjkim308@gmail.com>
2021-10-05 17:19:50 +0900

    commited all

--- Coding_homework/For_loop.cpp
+++ Coding_homework/For_loop.cpp
@@ -28,6 +28,16 @@ int main()
 	/***********************************
 	Implement the code here!
 	************************************/
+	int count=0;
+	for(int i=0; i<arrsize; i++){
+		count=0;
+		for(int j=0; j<arrsize; j++){
+			if(arr[j]<arr[i]){
+				count+=1;
+			}
+		}
+		output[i]=count;
+	}
 
 	for (int i = 0; i < arrsize; i++) {
 		cout << output[i] << " ";
